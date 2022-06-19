a4fc5751976d0c37487888d87b011ccf2dd3d4ce
ywan311 <ywan311@naver.com>
2022-06-19 16:45:07 +0900

    feature : study component

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const linkStyle ={
+    marginRight: 15
+}
+const Header = () =>(
+    <div>
+        <Link href="/">
+            <a style={linkStyle}> HOME </a>
+        </Link>
+
+        <Link href="/about">
+            <a style={linkStyle}> about </a>
+        </Link>
+    </div>
+)
+export default Header
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Header from "../components/Header"
+export default () => (
+    <div>
+        <Header/>
+        <a> This is About page.</a>
+    </div>
+)
