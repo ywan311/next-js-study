b2161c32aa5cc84345663b08f4fc3c02d5138623
ywan311 <ywan311@naver.com>
2022-07-30 12:40:37 +0900

    feature : Set json-server 리액트 쿼리를 통한 상태관리 연습을 위한 json server 세팅

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import axios from "axios"
+
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
+}
+
+
+function getOrderList(){
+  axios.get("")
 }
